9b9fe513a1d4c13d56b7dc056f173b0bb2ec399d
joffrey7486 <64081938+joffrey7486@users.noreply.github.com>
2021-08-06 11:41:44 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+// define all variable
+let form_input = document.getElementById('form_input')
+let input = document.getElementsByTagName('input')
+
+let firstName = document.getElementById('name')
+let lastName = document.getElementById('last_name')
+let email = document.getElementById('email')
+let password = document.getElementById('password')
+let submit = document.getElementById('submit')
+
+let form_error = document.getElementsByClassName('form_error')
+let error = document.getElementsByClassName('error')
+
+// Validation function
+function formValidation(event) {
+    // Use for loop for all input fields, except submit entry
+    for(let i = 0; i < 4; i++){
+        //clear the form 
+        input[i].classList.remove('error')
+        form_error[i].style.display = "none"
+
+        // email validation
+        if(!email.validity.valid){
+            form_error[2].style.display = 'block'
+            email.classList.add('error')
+            event.preventDefault()
+        }
+
+        // validation to other fields
+        if((input.type != 'email' || input.type != 'submit') && input[i].value == ''){
+            form_error[i].style.display = 'block'
+            input[i].classList.add('error')
+            event.preventDefault()
+        }
+    }
+}
+
+// event listerner on the button for call the function
+form_input.addEventListener('submit', formValidation, false);
